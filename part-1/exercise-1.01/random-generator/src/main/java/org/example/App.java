988f9df8e0f6f982c46009a99c750c4cbbdb0e70
Gabriel <damore120@gmail.com>
2024-07-06 17:46:07 -0300

    chore: exercise-1.01

--- part-1/exercise-1.01/random-generator/src/main/java/org/example/App.java
+++ part-1/exercise-1.01/random-generator/src/main/java/org/example/App.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import java.util.UUID;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws InterruptedException {
+        final var random = UUID.randomUUID();
+        do {
+            System.out.println( random );
+            Thread.sleep(5000);
+        } while (true);
+
     }
 }
